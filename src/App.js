5e027bb969e9d4bd018b92d065e8afe06c121248
Namo Narayan <namonarayan@Namos-MacBook-Pro.local>
2024-01-06 13:10:32 +0530

    backend url change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
 
-const socket=io.connect("http://localhost:4000")
+const socket=io.connect("https://chatapp-0ae8.onrender.com")
 
 function App() {
   const [username, setUsername] = useState("");
